5197e93fe0442f4b9b116df1764b197613592947
conner-replogle <connerlreplogle@gmail.com>
2023-03-06 09:39:47 -0600

    d

--- src/commands/schedule/summary.rs
+++ src/commands/schedule/summary.rs
@@ -1,11 +1,12 @@
 use arc_swap::ArcSwapAny;
 use chrono::{NaiveDateTime, Utc};
 use chrono_tz::Tz;
-use diesel::sql_types::Time;
+use diesel::{sql_types::Time, RunQueryDsl, QueryDsl};
 use serenity::{client::Context, model::prelude::command::CommandOptionType};
 use std::sync::Arc;
 use tracing::instrument;
 
+use diesel::ExpressionMethods;
 use serenity::{
     async_trait,
     builder::CreateApplicationCommand,
@@ -17,11 +18,11 @@ use serenity::{
 use crate::{
     commands::{AutoComplete, CommandError, SlashCommand},
     config::Config,
-    models::NewReservation,
+    models::{NewReservation, User, ClockStub, DBTransaction},
     ops::user_ops,
     schedule::{Schedule, TimeSlot},
     schema::reservations,
-    utils::get_option::{self, get_option},
+    utils::get_option::{self, get_option}, db::establish_connection,
 };
 pub(crate) const COMMAND_NAME: &'static str = "summary";
 pub(crate) fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
@@ -47,13 +48,70 @@ impl SlashCommand for SummaryCommand {
             ..Default::default()
         }
     }
-    #[instrument(skip_all, name = "Reserve Command", level = "trace")]
+    #[instrument(skip_all, name = "Summary Command", level = "trace")]
     async fn run(
         &self,
         interaction: ApplicationCommandInteraction,
         ctx: Context,
         config: Arc<ArcSwapAny<Arc<Config>>>,
     ) -> Result<(), CommandError> {
+        let mut connection = establish_connection();
+        let users;
+        //Get users
+        {
+            use crate::schema::users::dsl;
+            users = dsl::users.load::<User>(&mut connection)?;
+        }   
+        let mut pay = Vec::new();
+        for user in users.iter(){
+            let stubs;
+            {
+                use crate::schema::clock_stubs::dsl;
+                stubs = dsl::clock_stubs.filter(dsl::user_id.eq(user.id)).load::<ClockStub>(&mut connection)?;
+            }   
+            let mut total_earned = 0.0;
+            let mut total_mins = 0;
+            
+            for stub in stubs.iter(){
+                let Some(end_time) = stub.end_time else{
+                    continue;
+                };
+                let transactions;
+                {
+                    use crate::schema::transactions::dsl;
+                    transactions = dsl::transactions.filter(dsl::clock_stub_id.eq(stub.id)).load::<DBTransaction>(&mut connection)?;
+                } 
+                let mut stub_pay = 0.0;
+                for transaction in transactions.iter(){
+                    let Some(buy_price ) = transaction.buyAvgPrice else{
+                        continue;
+                    };
+                    let Some(sell_price ) = transaction.buyAvgPrice else{
+                        continue;
+                    };
+                    stub_pay += sell_price - buy_price
+                    
+                }
+                total_mins += (end_time - stub.start_time).num_minutes();
+                total_earned += stub_pay;
+
+            }
+            pay.push((user.tag.clone(),total_mins,total_earned));
+
+        }
+
+        interaction.edit_original_interaction_response(&ctx.http, |i| {
+            for (tag,mins,earned) in pay.iter(){
+                i.embed(|e|
+                    e.title(tag)
+                    .field("Mins", mins, true)
+                    .field("Earned", earned, true)
+                );
+            }
+            i
+        }
+        ).await?;
+
 
         
         Ok(())
